119e8999e6515c5c1c42867aabf4fa4fd5865188
Grant Surlyn <grant@sunflowerseastar.com>
2023-09-13 19:48:31 -0400

    Refactor Score component to display both the score and the high score.

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,13 +1,25 @@
 import { Len9Text } from "./Len9";
 
-const Score = ({ score }: { score: number }) => (
-  <div>
-    <Len9Text
-      text={score.toString()}
-      gridWidth={60}
-      isRightAligned
-    />
-  </div>
-);
+import { useContext } from 'react';
+import { GameContext } from '../App';
+
+const Score = ({ score }: { score: number }) => {
+  const { state } = useContext(GameContext);
+
+  return (
+    <div>
+      <Len9Text
+        text={score.toString()}
+        gridWidth={60}
+        isRightAligned
+      />
+      <Len9Text
+        text={state.highScore.toString()}
+        gridWidth={60}
+        isLeftAligned
+      />
+    </div>
+  );
+};
 
 export default Score;
