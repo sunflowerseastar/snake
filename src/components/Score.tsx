e10be0ddabd9f823ff90c9373f8df0442fea37e8
Grant Surlyn <grant@sunflowerseastar.com>
2023-09-21 22:27:53 -0400

    fixup score css

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,10 +1,10 @@
 import { Len9Text } from "./Len9";
 
 const Score = ({ highScore, score }: { highScore: number; score: number }) => (
-  <div>
-    <Len9Text text={highScore.toString()} gridWidth={60} />
-    <Len9Text text={score.toString()} gridWidth={60} isRightAligned />
-  </div>
+  <>
+    <Len9Text text={highScore.toString()} gridWidth={30} />
+    <Len9Text text={score.toString()} gridWidth={30} isRightAligned />
+  </>
 );
 
 export default Score;
