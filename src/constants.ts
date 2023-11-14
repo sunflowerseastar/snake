bb03f1d1f19d500582552033263d05b1cf9b15f6
Grant Surlyn <grant@sunflowerseastar.com>
2023-11-14 00:13:39 -0500

    adjust starting speed

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const FALLBACK_INTERVAL_MS = 60; // snake speed
-export const CRASHFLASH_INTERVAL_MS = 460;
+export const FALLBACK_INTERVAL_MS = 100; // snake speed
+export const CRASHFLASH_INTERVAL_MS = 460; // flashing speed at game over
